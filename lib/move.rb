67877a599071ea64ca690cb494de2aa67b15aada
abstract-animation-9914 <vm930@hotmail.com>
2019-02-07 16:19:41 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,6 +11,6 @@ def input_to_index(input)
    input.to_i - 1
 end 
 
-def move(board, broad_index, character = "X")
-  board[broad_index] = character
+def move(board, board_index, character = "X")
+  board[board_index] = character
 end 
